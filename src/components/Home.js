a0976cbe4fd5cc6d307db197e8d935d2240eb098
Faycal ZOUAOUI <faycalzouaoui@MAC519.home>
2021-01-11 13:46:16 +0100

    changes

---
+++ src/components/Home.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import Emoji from "react-emoji-render";
+
+
+
+class Home extends React.Component {
+    render() {
+    return (
+        <div className="App-header">
+            <h1 className="Home"><Emoji text=":+1:  Well Done :)" /> </h1>
+            
+
+            
+        </div>
+
+    );
+    }
+}
+
+
+
+
+
+export default Home
